3a320989af552ccc9aa2b319f2c01377f8a93202
viettran0801 <viettran0801@gmail.com>
2025-07-18 20:24:26 +0700

    Refactor project structure for nl2sql experiment and add output format converter script
    - Rename project to `nl2sql_flow` and update references in `pyproject.toml`.
    - Adjust `.gitignore` to include SQL files.
    - Replace `guide_creator_flow` package with `nl2sql_flow` in `uv.lock`.
    - Add `convert-output-format.py` script for processing NL2SQL experiment results.

---
+++ experiments/experiment3_multi_agent_crewai/convert-output-format.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+if __name__ == "__main__":
+    with open("output/nl2sql_results_20250718171820.csv", "r", encoding='utf-8') as f:
+        fieldnames = ['db_id', 'question', 'sql', 'explain', 'error']
+        result_reader = csv.DictReader(f, fieldnames=fieldnames)
+        with open('train_spider.json', 'r', encoding='utf-8') as fq:
+            questions = json.load(fq)
+            with open('predict.sql', 'w', encoding='utf-8') as output_predict_file:
+                with open('gold.sql', 'w', encoding='utf-8') as output_gold_file:
+                    for row_num, row in enumerate(result_reader, 1):
+                        for question in questions:
+                            if question['question'] == row['question']:
+                                output_gold_file.write(f"{question['query']}	  {question['db_id']}\n")
+                                output_predict_file.write(f"{row['sql']}	  {row['db_id']}\n")
+                                break
